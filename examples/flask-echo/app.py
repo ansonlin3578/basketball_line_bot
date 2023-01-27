f56d13ee7fd56249c8b653f5eb06084499f7c01c
ansonlin0722@gmail.com <sungyulin@DESKTOP-AKJSEQN.localdomain>
2023-01-27 21:00:30 +0800

    ori

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -65,7 +65,6 @@ def callback():
         if not isinstance(event.message, TextMessage):
             continue
 
-
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text)
